78f4765b0e3a488aee6a3a9568cc82464ffdd580
ksy8888 <ksy8_13@naver.com>
2024-01-22 20:25:31 +0900

    0122_문자열 문제 start

---
+++ 백준알고리즘/src/문자열/문자와문자열/문자와문자열_27866.java
@@ -0,0 +1,20 @@
+package 문자열.문자와문자열;
+
+import java.io.*;
+
+public class 문자와문자열_27866 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine(); //Sprout 
+		int I = Integer.parseInt(br.readLine()); //3
+		
+		for(int i=1; i<=S.length(); i++) {
+			if(i == I) {
+				System.out.println(S.substring(I-1,I));
+			}
+		}
+	}
+
+}
